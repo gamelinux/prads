b2819196c1a636513f05abdb191fb0204ad6082f
Edward Bjarte Fjellskål <git@pronix.no>
2009-02-16 15:03:33 +0100

    Changed some values in OSfingerprinting, and added a unknown

--- sbin/prads.pl
+++ sbin/prads.pl
@@ -155,20 +155,23 @@ sub syn_packets {
       ##### THIS IS WHERE THE PASSIVE OS FINGERPRINTING MAGIC SHOULD BE
       warn "OS: ip:$ip->{'src_ip'} ttl=$ttl, DF=$fragment, ipflags=$ipflags, winsize=$winsize, tcpflags=$tcpflags, tcpoptsinhex=$hex\n" if($DEBUG);
       # Bogus/weak test, PoC - REWRITE
-      if((64 >= $ttl) && ($ttl > 24)) {
+      if((64 >= $ttl) && ($ttl > 32)) {
          if ($fragment == 1) {
             if((5840 >= $winsize) && ($winsize >= 5488)) {
                print "OS Fingerprint: $ip->{'src_ip'}:$tcp->{'src_port'} - Linux 2.6 (up: 39 hrs) \n";
-               print "                $ip->{'dest_ip'}:$tcp->{'dest_port'} - (distance 9, link: ethernet/modem) \n";
+               print "                $ip->{'dest_ip'}:$tcp->{'dest_port'} - (ttl: $ttl, link: ethernet/modem) \n";
             }
          }
       }elsif ((128 >= $ttl) && ($ttl > 64)) {
         if ($fragment == 1) {
-           if((65535 > $winsize ) && ($winsize >= 64800)) {
+           if((65535 > $winsize ) && ($winsize >= 60000)) {
                print "OS Fingerprint: $ip->{'src_ip'}:$tcp->{'src_port'} - Windows 2000/2003/XP \n";
-               print "                $ip->{'dest_ip'}:$tcp->{'dest_port'} - (distance 13, link: IPv6/IPIP) \n";
+               print "                $ip->{'dest_ip'}:$tcp->{'dest_port'} - (ttl: $ttl, link: IPv6/IPIP) \n";
             }
          }
+      }else{
+               print "OS Fingerprint: $ip->{'src_ip'}:$tcp->{'src_port'} - UNNKOWN \n";
+               print "                $ip->{'dest_ip'}:$tcp->{'dest_port'} - (ttl: $ttl, link: IPv4/PPoE) \n";
       }
  
     }else{
