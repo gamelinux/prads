3ea48a94aa229036976b858185cdb11b6853f376
Edward <edward@aruba.linpro.no>
2009-06-04 22:24:09 +0200

    Added an example for the www-gui ip search

---
+++ www/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+<link rel="icon" href="./favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+<title>PRADS WEB</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<BODY>
+ <form METHOD='post' NAME='IP' ACTION=''>
+  <div align='LEFT'><TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH='100%' BGCOLOR='#FFFFFF' >
+   <TR><TD width='200'><B>IP Address:</B></TD><TD><input TYPE='test' SIZE='15' NAME='ip' Value=""><input TYPE='submit' NAME='submit' VALUE='Search'></TD></TR>
+  </div>
+  <BR>
+  <TABLE BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH='100%' BGCOLOR='#FFFFFF' ><BR>
+   <TR>
+    <TD CLASS='plfieldhdrleft' WIDTH='10%'> IP </TD>
+    <TD CLASS='plfieldhdrleft' WIDTH='10%'> DETECTION </TD>
+    <TD CLASS='plfieldhdrleft' WIDTH='10%'> OS/SERVICE </TD>
+    <TD CLASS='plfieldhdrleft' WIDTH='20%'> DETAILS </TD>
+    <TD CLASS='plfieldhdrleft' WIDTH='10%'> LINK </TD>
+    <TD CLASS='plfieldhdrleft' WIDTH='10%'> DISTANCE </TD>
+    <TD CLASS='plfieldhdrleft' WIDTH='10%'> TIMESTAMP </TD>
+   </TR>
+   <TR BGCOLOR="#DDDDDD">
+    <TD ALIGN=LEFT>&nbsp;<font>81.31.233.9</font>&nbsp;</TD>
+    <TD ALIGN=LEFT>&nbsp;ICMP</TD>
+    <TD ALIGN=LEFT>&nbsp;Linux</TD>
+    <TD ALIGN=LEFT>&nbsp;2.6</TD>
+    <TD ALIGN=LEFT>&nbsp;ethernet/modem</TD>
+    <TD ALIGN=LEFT>&nbsp;1</TD>
+    <TD ALIGN=LEFT>&nbsp;1244100867</TD>
+   </TR>
+   <TR BGCOLOR="#FFFFFF">
+    <TD ALIGN=LEFT>&nbsp;<font>81.31.233.9</font>&nbsp;</TD>
+    <TD ALIGN=LEFT>&nbsp;UDP</TD>
+    <TD ALIGN=LEFT>&nbsp;Linux</TD>
+    <TD ALIGN=LEFT>&nbsp;2.6</TD>
+    <TD ALIGN=LEFT>&nbsp;ethernet/modem</TD>
+    <TD ALIGN=LEFT>&nbsp;1</TD>
+    <TD ALIGN=LEFT>&nbsp;1244100889</TD>
+   </TR>
+   <TR BGCOLOR="#DDDDDD">
+    <TD ALIGN=LEFT>&nbsp;<font>81.31.233.9</font>&nbsp;</TD>
+    <TD ALIGN=LEFT>&nbsp;SERVICE_TCP</TD>
+    <TD ALIGN=LEFT>&nbsp;Apache</TD>
+    <TD ALIGN=LEFT>&nbsp;2.2.4</TD>
+    <TD ALIGN=LEFT>&nbsp;ethernet/modem</TD>
+    <TD ALIGN=LEFT>&nbsp;1</TD>
+    <TD ALIGN=LEFT>&nbsp;1244100917</TD>
+   </TR>
+   <TR BGCOLOR="#FFFFFF">
+    <TD ALIGN=LEFT>&nbsp;<font>81.31.233.9</font>&nbsp;</TD>
+    <TD ALIGN=LEFT>&nbsp;SERVICE_TCP</TD>
+    <TD ALIGN=LEFT>&nbsp;Postfix</TD>
+    <TD ALIGN=LEFT>&nbsp;cuba.pronix.no SMPT</TD>
+    <TD ALIGN=LEFT>&nbsp;ethernet/modem</TD>
+    <TD ALIGN=LEFT>&nbsp;1</TD>
+    <TD ALIGN=LEFT>&nbsp;1244100881</TD>
+   </TR>
+   <TR BGCOLOR="#DDDDDD">
+    <TD ALIGN=LEFT>&nbsp;<font>81.31.233.9</font>&nbsp;</TD>
+    <TD ALIGN=LEFT>&nbsp;SYN</TD>
+    <TD ALIGN=LEFT>&nbsp;Linux</TD>
+    <TD ALIGN=LEFT>&nbsp;2.6 (newer, 6)</TD>
+    <TD ALIGN=LEFT>&nbsp;ethernet/modem</TD>
+    <TD ALIGN=LEFT>&nbsp;1</TD>
+    <TD ALIGN=LEFT>&nbsp;1244100912</TD>
+   </TR>
+   <TR BGCOLOR="#FFFFFF">
+    <TD ALIGN=LEFT>&nbsp;<font>81.31.233.9</font>&nbsp;</TD>
+    <TD ALIGN=LEFT>&nbsp;SYNACK</TD>
+    <TD ALIGN=LEFT>&nbsp;Linux</TD>
+    <TD ALIGN=LEFT>&nbsp;2.6 (newer, 6)</TD>
+    <TD ALIGN=LEFT>&nbsp;ethernet/modem</TD>
+    <TD ALIGN=LEFT>&nbsp;1</TD>
+    <TD ALIGN=LEFT>&nbsp;1244100913</TD>
+   </TR>
+  </TABLE>
+  <TR BGCOLOR="#FFFFFF">
+   <TD ALIGN=LEFT><BR></TD>
+  </TR>
+  <TR BGCOLOR="#DDDDDD">
+   <TD ALIGN=LEFT>&nbsp;OS GUESS (100%): Linux 2.6</TD>
+  </TR>
+  <INPUT TYPE="hidden" NAME="ip" VALUE="0">
+ </form>
+
+ <BR>
+ <h6>"PRADS - Know your assets"</h6>
+</BODY>
+
+</html>
