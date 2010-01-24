ee2213049007d84c4ea7dcba18dedf5759dfd11c
Edward Bjarte Fjellskål <edward.fjellskaal@redpill-linpro.com>
2010-01-24 22:30:02 +0100

    changed the some time values back to something more sain

--- src/prads.h
+++ src/prads.h
@@ -30,9 +30,9 @@
 
 /*  D E F I N E S  ************************************************************/
 #define VERSION                       "0.1.7"
-#define CHECK_TIMEOUT                 60       /* Time between cxt and asset cleaning/printing */
+#define CHECK_TIMEOUT                 600       /* Time between cxt and asset cleaning/printing */
 #define TCP_TIMEOUT                   300       /* When idle IP connections should be timed out */
-#define ASSET_TIMEOUT                 180      /* Time befor an asset is deleted if no updates */
+#define ASSET_TIMEOUT                 1800      /* Time befor an asset is deleted if no updates */
 #define BUCKET_SIZE                   1669
 #define SNAPLENGTH                    1604
 #define MAX_BYTE_CHECK                5000000
