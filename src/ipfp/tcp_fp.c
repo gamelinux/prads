f2b70753901b9e4ed7582877c68adf373e4293f2
Edward Bjarte Fjellskål <edward.fjellskaal@redpill-linpro.com>
2010-02-09 09:33:08 +0100

    skipping find_match for ipv6 for now

--- src/ipfp/tcp_fp.c
+++ src/ipfp/tcp_fp.c
@@ -232,6 +232,7 @@ void fp_tcp(packetinfo *pi, uint8_t ftype)
                tstamp, e.quirks, ftype, pi);
     // find_match(pi, e);
     // return this into asset engine
+    if (pi->ip6 != NULL) return; // Fix this when find_match() is IPv6 aware
     find_match(e.size,
                e.df,
                e.ttl,
