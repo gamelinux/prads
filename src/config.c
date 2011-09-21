3e55d74de8a4d26cbd07186d8024e58174ea6533
Kacper Wysocki <kwy@redpill-linpro.com>
2011-09-21 18:05:27 +0200

    refactor args parsing and initialization
    refactored arg parsing into parse_args() in config.c
    prads_initialize() now handles acting on the configuration.
    globalconfig is now passed and accessed through pointers (for a potential future thread-based system)

--- src/config.c
+++ src/config.c
@@ -34,23 +34,23 @@
 extern globalconfig config;
 
 /* F U N C T I O N S  ********************************************************/
-void display_config()
+void display_config(globalconfig *conf)
 {
     olog("[*] OS checks enabled:");
-    if (IS_COSET(&config,CO_SYN))    olog (" SYN");
-    if (IS_COSET(&config,CO_SYNACK)) olog (" SYNACK");
-    if (IS_COSET(&config,CO_RST))    olog (" RST");
-    if (IS_COSET(&config,CO_FIN))    olog (" FIN");
-    if (IS_COSET(&config,CO_ACK))    olog (" ACK");
+    if (IS_COSET(conf,CO_SYN))    olog (" SYN");
+    if (IS_COSET(conf,CO_SYNACK)) olog (" SYNACK");
+    if (IS_COSET(conf,CO_RST))    olog (" RST");
+    if (IS_COSET(conf,CO_FIN))    olog (" FIN");
+    if (IS_COSET(conf,CO_ACK))    olog (" ACK");
     olog("\n");
     
     olog("[*] Service checks enabled:");
-    if (IS_CSSET(&config,CS_TCP_SERVER))    olog (" TCP-SERVER");
-    if (IS_CSSET(&config,CS_TCP_CLIENT))    olog (" TCP-CLIENT");
-    if (IS_CSSET(&config,CS_UDP_SERVICES))  olog (" UDP-SERVICES");
-    if (IS_CSSET(&config,CS_ICMP))          olog (" ICMP");
-    if (IS_CSSET(&config,CS_ARP))           olog (" ARP");
-    if (IS_CSSET(&config,CS_MAC))           olog (" MAC");
+    if (IS_CSSET(conf,CS_TCP_SERVER))    olog (" TCP-SERVER");
+    if (IS_CSSET(conf,CS_TCP_CLIENT))    olog (" TCP-CLIENT");
+    if (IS_CSSET(conf,CS_UDP_SERVICES))  olog (" UDP-SERVICES");
+    if (IS_CSSET(conf,CS_ICMP))          olog (" ICMP");
+    if (IS_CSSET(conf,CS_ARP))           olog (" ARP");
+    if (IS_CSSET(conf,CS_MAC))           olog (" MAC");
     olog("\n");
 
     return;
@@ -66,56 +66,56 @@ void free_config()
     if (config.bpff != NULL) free(config.bpff);
 }
 
-void set_default_config_options()
+void set_default_config_options(globalconfig *config)
 {
-    config.file    = CONFDIR "prads.conf";
-    config.ctf    |= CO_SYN;
-    config.ctf    |= CO_RST;
-    config.ctf    |= CO_FIN;
-    config.ctf    |= CO_ACK;
-    config.ctf    |= CO_SYNACK;
-    //config.ctf    |= CO_ICMP;
-    //config.ctf    |= CO_UDP;
-    //config.ctf    |= CO_OTHER;
-    config.cof    |= CS_TCP_SERVER;
-    config.cof    |= CS_TCP_CLIENT;
-    config.cof    |= CS_UDP_SERVICES;
-    config.cof    |= CS_MAC;
-    config.dev     = 0x0; // default is to lookup device
-    config.bpff    = strdup("");
-    //config.pidfile = strdup("/var/run/prads.pid");
-    config.assetlog= strdup(LOGDIR PRADS_ASSETLOG);
-    config.fifo    = NULL;
+    config->file    = CONFDIR "prads.conf";
+    config->ctf    |= CO_SYN;
+    config->ctf    |= CO_RST;
+    config->ctf    |= CO_FIN;
+    config->ctf    |= CO_ACK;
+    config->ctf    |= CO_SYNACK;
+    //config->ctf    |= CO_ICMP;
+    //config->ctf    |= CO_UDP;
+    //config->ctf    |= CO_OTHER;
+    config->cof    |= CS_TCP_SERVER;
+    config->cof    |= CS_TCP_CLIENT;
+    config->cof    |= CS_UDP_SERVICES;
+    config->cof    |= CS_MAC;
+    config->dev     = 0x0; // default is to lookup device
+    config->bpff    = strdup("");
+    //config->pidfile = strdup("/var/run/prads.pid");
+    config->assetlog= strdup(LOGDIR PRADS_ASSETLOG);
+    config->fifo    = NULL;
     // default source net owns everything
-    config.s_net   = DEFAULT_NETS ;
-    config.errbuf[0] = '\0';
-    config.configpath = CONFDIR "";
+    config->s_net   = DEFAULT_NETS ;
+    config->errbuf[0] = '\0';
+    config->configpath = CONFDIR "";
     // files should be relative to configpath somehow
-    config.sig_file_syn = CONFDIR "tcp-syn.fp";
-    config.sig_file_synack = CONFDIR "tcp-synack.fp";
-    config.sig_file_ack = CONFDIR "tcp-stray-ack.fp";
-    config.sig_file_fin = CONFDIR "tcp-fin.fp";
-    config.sig_file_rst = CONFDIR "tcp-rst.fp";
-    config.sig_file_mac = CONFDIR "mac.sig";
-    config.sig_file_serv_tcp = CONFDIR "tcp-service.sig";
-    config.sig_file_serv_udp = CONFDIR "udp-service.sig";
-    config.sig_file_cli_tcp = CONFDIR "tcp-clients.sig";
-    config.sig_syn = NULL;
-    config.sig_synack = NULL;
-    config.sig_ack = NULL;
-    config.sig_fin = NULL;
-    config.sig_rst = NULL;
-    config.sig_mac = NULL;
-    config.sig_hashsize = SIG_HASHSIZE;
-    config.mac_hashsize = MAC_HASHSIZE;
+    config->sig_file_syn = CONFDIR "tcp-syn.fp";
+    config->sig_file_synack = CONFDIR "tcp-synack.fp";
+    config->sig_file_ack = CONFDIR "tcp-stray-ack.fp";
+    config->sig_file_fin = CONFDIR "tcp-fin.fp";
+    config->sig_file_rst = CONFDIR "tcp-rst.fp";
+    config->sig_file_mac = CONFDIR "mac.sig";
+    config->sig_file_serv_tcp = CONFDIR "tcp-service.sig";
+    config->sig_file_serv_udp = CONFDIR "udp-service.sig";
+    config->sig_file_cli_tcp = CONFDIR "tcp-clients.sig";
+    config->sig_syn = NULL;
+    config->sig_synack = NULL;
+    config->sig_ack = NULL;
+    config->sig_fin = NULL;
+    config->sig_rst = NULL;
+    config->sig_mac = NULL;
+    config->sig_hashsize = SIG_HASHSIZE;
+    config->mac_hashsize = MAC_HASHSIZE;
     // drop privileges by default
-    config.user_name = strdup("1");
-    config.group_name = strdup("1");
+    config->user_name = strdup("1");
+    config->group_name = strdup("1");
 
-    config.drop_privs_flag = 1;
+    config->drop_privs_flag = 1;
     // don't chroot or daemonize by default
-    config.chroot_dir = NULL;
-    config.daemon_flag = 0;
+    config->chroot_dir = NULL;
+    config->daemon_flag = 0;
 }
 
 void parse_config_file(const char* fname)
@@ -134,7 +134,7 @@ void parse_config_file(const char* fname)
     filedata = bread ((bNread) fread, fp);
     if ((lines = bsplit(filedata, '\n')) != NULL) {
         for (i = 0; i < lines->qty; i++) {
-            parse_line(lines->entry[i]);
+            parse_line(&config, lines->entry[i]);
         }
     }
 
@@ -143,7 +143,7 @@ void parse_config_file(const char* fname)
     fclose(fp);
 }
 
-void parse_line (bstring line)
+void parse_line (globalconfig * conf, bstring line)
 {
     bstring param, value;
     struct bstrList *list;
@@ -180,149 +180,149 @@ void parse_line (bstring line)
     /* Do something based upon value. */
     if ((biseqcstr(param, "daemon")) == 1) {
         /* DAEMON */
-        if (!config.daemon_flag) {
+        if (!conf->daemon_flag) {
             if (value->data[0] == '1')
-                config.daemon_flag = 1;
+                conf->daemon_flag = 1;
             else
-                config.daemon_flag = 0;
+                conf->daemon_flag = 0;
         }
     } else if ((biseqcstr(param, "mac")) == 1) {
         /* MAC CHECK */
         if (value->data[0] == '1')
-            config.cof |= CS_MAC;
+            conf->cof |= CS_MAC;
         else 
-            config.cof &= ~CS_MAC;
+            conf->cof &= ~CS_MAC;
     } else if ((biseqcstr(param, "arp")) == 1) {
         /* ARP CHECK */
         if (value->data[0] == '1')
-            config.cof |= CS_ARP;
+            conf->cof |= CS_ARP;
         else 
-            config.cof &= ~CS_ARP;
+            conf->cof &= ~CS_ARP;
     } else if ((biseqcstr(param, "service_tcp")) == 1) {
         /* TCP Service check */
         if (value->data[0] == '1')
-            config.cof |= CS_TCP_SERVER;
+            conf->cof |= CS_TCP_SERVER;
         else
-            config.cof &= ~CS_TCP_SERVER;
+            conf->cof &= ~CS_TCP_SERVER;
     } else if ((biseqcstr(param, "client_tcp")) == 1) {
         /* TCP Client check */
         if (value->data[0] == '1')
-            config.cof |= CS_TCP_CLIENT;
+            conf->cof |= CS_TCP_CLIENT;
         else
-            config.cof &= ~CS_TCP_CLIENT;
+            conf->cof &= ~CS_TCP_CLIENT;
     } else if ((biseqcstr(param, "service_udp")) == 1) {
         /* UPD service and client checks */
         if (value->data[0] == '1')
-            config.cof |= CS_UDP_SERVICES;
+            conf->cof |= CS_UDP_SERVICES;
         else
-            config.cof &= ~CS_UDP_SERVICES;
+            conf->cof &= ~CS_UDP_SERVICES;
     } else if ((biseqcstr(param, "os_icmp")) == 1) {
         /* ICMP OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_ICMP;
+            conf->ctf |= CO_ICMP;
         else
-            config.ctf &= ~CO_ICMP;
+            conf->ctf &= ~CO_ICMP;
    } else if ((biseqcstr(param, "os_udp")) == 1) {
         /* UDP OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_UDP;
+            conf->ctf |= CO_UDP;
         else
-            config.ctf &= ~CO_UDP;
+            conf->ctf &= ~CO_UDP;
     } else if ((biseqcstr(param, "service_udp")) == 1) {
         /* UPD service and client checks */
         if (value->data[0] == '1')
-            config.cof |= CS_UDP_SERVICES;
+            conf->cof |= CS_UDP_SERVICES;
         else
-            config.cof &= ~CS_UDP_SERVICES;
+            conf->cof &= ~CS_UDP_SERVICES;
    } else if ((biseqcstr(param, "os_syn_fingerprint")) == 1) {
         /* TCP SYN OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_SYN;
+            conf->ctf |= CO_SYN;
         else
-            config.ctf &= ~CO_SYN;
+            conf->ctf &= ~CO_SYN;
    } else if ((biseqcstr(param, "os_synack_fingerprint")) == 1) {
         /* TCP SYNACK OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_SYNACK;
+            conf->ctf |= CO_SYNACK;
         else
-            config.ctf &= ~CO_SYNACK;
+            conf->ctf &= ~CO_SYNACK;
    } else if ((biseqcstr(param, "os_ack_fingerprint")) == 1) {
         /* TCP Stray ACK OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_ACK;
+            conf->ctf |= CO_ACK;
         else
-            config.ctf &= ~CO_ACK;
+            conf->ctf &= ~CO_ACK;
    } else if ((biseqcstr(param, "os_rst_fingerprint")) == 1) {
         /* TCP RST OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_RST;
+            conf->ctf |= CO_RST;
         else
-            config.ctf &= ~CO_RST;
+            conf->ctf &= ~CO_RST;
    } else if ((biseqcstr(param, "os_fin_fingerprint")) == 1) {
         /* TCP FIN OS Fingerprinting */
         if (value->data[0] == '1')
-            config.ctf |= CO_FIN;
+            conf->ctf |= CO_FIN;
         else
-            config.ctf &= ~CO_FIN;
+            conf->ctf &= ~CO_FIN;
    } else if ((biseqcstr(param, "chroot_dir")) == 1) {
         /* CHROOT DIRECTORY */
-        if(config.chroot_dir) free(config.chroot_dir);
-        config.chroot_dir = bstr2cstr(value, '-');
+        if(conf->chroot_dir) free(conf->chroot_dir);
+        conf->chroot_dir = bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "pid_file")) == 1) {
         /* PID FILE */
-        if(config.pidfile) free(config.pidfile);
-        config.pidfile = bstr2cstr(value, '-');
+        if(conf->pidfile) free(conf->pidfile);
+        conf->pidfile = bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "asset_log")) == 1) {
         /* PRADS ASSET LOG */
-        if(config.assetlog) free(config.assetlog);
-        config.assetlog = bstr2cstr(value,'-');
+        if(conf->assetlog) free(conf->assetlog);
+        conf->assetlog = bstr2cstr(value,'-');
     } else if ((biseqcstr(param, "fifo")) == 1) {
         /* FIFO path */
-        config.fifo = bstr2cstr (value, '-');
+        conf->fifo = bstr2cstr (value, '-');
     } else if ((biseqcstr(param, "sig_file_serv_tcp")) == 1) {
         /* SIGNATURE FILE */
-        config.sig_file_serv_tcp = bstr2cstr(value, '-');
+        conf->sig_file_serv_tcp = bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "sig_file_cli_tcp")) == 1) {
         /* SIGNATURE FILE */
-        config.sig_file_cli_tcp =  bstr2cstr(value, '-');
+        conf->sig_file_cli_tcp =  bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "sig_file_serv_udp")) == 1) {
         /* SIGNATURE FILE */
-        config.sig_file_serv_udp = bstr2cstr(value, '-');
+        conf->sig_file_serv_udp = bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "sig_file_cli_udp")) == 1) {
         /* SIGNATURE FILE */
-        config.sig_file_cli_udp =  bstr2cstr(value, '-');
+        conf->sig_file_cli_udp =  bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "mac_file")) == 1) {
         /* MAC / VENDOR RESOLUTION FILE */
-        config.sig_file_mac = bstr2cstr(value, '-');
+        conf->sig_file_mac = bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "output")) == 1) {
         /* OUTPUT */
         //conf_module_plugin(value, &activate_output_plugin);
     } else if ((biseqcstr(param, "user")) == 1) {
         /* USER */
-        config.user_name = bstr2cstr(value, '-');
-        config.drop_privs_flag = 1;
+        conf->user_name = bstr2cstr(value, '-');
+        conf->drop_privs_flag = 1;
     } else if ((biseqcstr(param, "group")) == 1) {
         /* GROUP */
-        config.group_name = bstr2cstr(value, '-');
-        config.drop_privs_flag = 1;
+        conf->group_name = bstr2cstr(value, '-');
+        conf->drop_privs_flag = 1;
     } else if ((biseqcstr(param, "interface")) == 1) {
         /* INTERFACE */
-        config.dev = bstr2cstr(value, '-');
+        conf->dev = bstr2cstr(value, '-');
     } else if ((biseqcstr(param, "bpfilter")) == 1) {
         /* FILTER */
-        free(config.bpff);
-        config.bpff = bstr2cstr(value, '-');
+        free(conf->bpff);
+        conf->bpff = bstr2cstr(value, '-');
 
 //    } else if ((biseqcstr(param, "network")) == 1) {
 //        /* NETWORK */
 //        parse_networks((unsigned char *)bdata(value));
 //    } else if ((biseqcstr(param, "hide_unknowns")) == 1) {
 //        /* UNKNOWN */
-//        if (!config.hide_unknowns) {
+//        if (!conf->hide_unknowns) {
 //            if (value->data[0] == '1')
-//                config.hide_unknowns = 1;
+//                conf->hide_unknowns = 1;
 //            else
-//                config.hide_unknowns = 0;
+//                conf->hide_unknowns = 0;
 //        }
     }
 
@@ -384,6 +384,125 @@ int brtrim (bstring string)
     if (len > 0)
     bdelete(string, i + 1, len);
 
+    return 0;
+}
+
+/* Parse commandline and make conf reflect this */
+int parse_args(globalconfig *conf, int argc, char *argv[], char *args)
+{
+    int ch = 0;
+    while ((ch = getopt(argc, argv, args)) != -1) {
+        switch (ch) {
+        case 'a':
+            if(strlen(optarg) == 0)
+                conf->s_net = DEFAULT_NETS;
+            else
+                conf->s_net = strdup(optarg);
+            break;
+        case 'c':
+            // too late at this point
+            conf->file = optarg; 
+            break;
+        case 'C':
+            conf->chroot_dir = strdup(optarg);
+            break;
+        case 'i':
+            conf->dev = optarg;
+            break;
+        case 'r':
+            conf->pcap_file = strdup(optarg);
+            break;
+        case 'b':
+            conf->bpff = strdup(optarg);
+            break;
+        case 'v':
+            conf->verbose++;
+            conf->cflags |= CONFIG_VERBOSE;
+            break;
+        case 'h':
+            return 1; // usage
+            break;
+        case 'D':
+            conf->daemon_flag = 1;
+            conf->cflags |= CONFIG_SYSLOG;
+            break;
+        case 'u':
+            conf->user_name = strdup(optarg);
+            conf->drop_privs_flag = 1;
+            break;
+        case 'g':
+            conf->group_name = strdup(optarg);
+            conf->drop_privs_flag = 1;
+            break;
+        case 'd':
+            conf->drop_privs_flag = 0;
+            break;
+        case 'p':
+            conf->pidfile = strdup(optarg);
+            break;
+        case 'l':
+            conf->assetlog = strdup(optarg);
+            break;
+        case 'f':
+            conf->fifo = strdup(optarg);
+            break;
+        case 's':
+            conf->payload = strtol(optarg, NULL, 0);
+            break;
+        case 'q':
+            conf->cflags |= CONFIG_QUIET;
+            break;
+        case 'O':
+            conf->cflags |= CONFIG_CONNECT;
+            break;
+        case 'Z':
+            conf->cflags |= CONFIG_PDNS;
+            break;
+        case 'x':
+            conf->cflags |= CONFIG_CXWRITE;
+            break;
+        case 'X':
+            conf->ctf = 0;
+            conf->cof = 0;
+            break;
+        case 'F':
+            conf->ctf |= CO_FIN;
+            break;
+        case 'R':
+            conf->ctf |= CO_RST;
+            break;
+        case 'M':
+            conf->cof |= CS_MAC;
+            conf->cof |= CS_ARP;
+            break;
+        case 'S':
+            conf->ctf |= CO_SYN;
+            break;
+        case 'A':
+            conf->ctf |= CO_ACK;
+            break;
+        case 'K':
+            conf->ctf |= CO_SYNACK;
+            break;
+        case 'U':
+            conf->cof |= CS_UDP_SERVICES;
+            break;
+        case 'T':
+            conf->cof |= CS_TCP_SERVER;
+            break;
+        case 'I':
+            conf->cof |= CS_ICMP;
+            break;
+        case 't':
+            conf->cof |= CS_TCP_CLIENT;
+            break;
+        case '?':
+            elog("unrecognized argument: '%c'\n", optopt);
+            break;
+        default:
+            elog("Did not recognize argument '%c'\n", ch);
+        }
+    }
     return 0;
 }
 
