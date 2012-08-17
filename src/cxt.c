960143fa1a1102adb6ac5e7bd88cbdad3f413b4a
Comotion <comotion@krutt.org>
2012-08-17 13:19:59 +0200

    bugfix: ugly but almost unnoticable snafu

--- src/cxt.c
+++ src/cxt.c
@@ -64,7 +64,6 @@ int connection_tracking(packetinfo *pi)
 {
     static char ip_addr_s[INET6_ADDRSTRLEN];
     static char ip_addr_d[INET6_ADDRSTRLEN];
-    struct in_addr *ipa; 
     cx_track(pi);
 
     if(config.cflags & CONFIG_CONNECT){
@@ -72,10 +71,8 @@ int connection_tracking(packetinfo *pi)
             u_ntop(pi->ip6->ip_src, pi->af, ip_addr_s);
             u_ntop(pi->ip6->ip_dst, pi->af, ip_addr_d);
         } else {
-            ipa = pi->ip4->ip_src;
-            inet_ntop(pi->af, &ipa, ip_addr_s, INET6_ADDRSTRLEN);
-            ipa = pi->ip4->ip_dst;
-            inet_ntop(pi->af, &ipa, ip_addr_d, INET6_ADDRSTRLEN);
+            inet_ntop(pi->af, &pi->ip4->ip_src, ip_addr_s, INET6_ADDRSTRLEN);
+            inet_ntop(pi->af, &pi->ip4->ip_dst, ip_addr_d, INET6_ADDRSTRLEN);
         }
         printf("conn[%4lu] %s:%u -> %s:%u [%s]\n", pi->cxt->cxid, 
                ip_addr_s, ntohs(pi->s_port),
